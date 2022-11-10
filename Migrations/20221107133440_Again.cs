a2bc7c97cfe3f0de5db78ef2897dc055764f294d
mseshiAyanda <mseshidladla@gmail.com>
2022-11-11 01:30:25 +0200

    Add project files.

---
+++ Migrations/20221107133440_Again.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ETicketing.Migrations
+{
+    public partial class Again : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Migrations/20221110223550_Initialsecond.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ETicketing.Migrations
+{
+    public partial class Initialsecond : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
